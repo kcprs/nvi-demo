6cc7f2fa4484be552f3af098117f6d1cbe9a9d96
Kacper Sagnowski <ksagnowski@gmail.com>
2023-09-22 13:57:04 +0200

    Complete basic demo

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
-int main(int /*unused*/, char ** /*unused*/) {}
+// Demo of the Non-Virtual Interface (NVI) idiom
+//
+// Resources:
+// http://www.gotw.ca/publications/mill18.htm
+// https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-Virtual_Interface
+
+#include <iostream>
+
+class IBase {
+public:
+  void foo() const { foo_impl(); }
+  virtual ~IBase() = default;
+
+private:
+  virtual void foo_impl() const = 0;
+};
+
+class DerivedA : public IBase {
+private:
+  void foo_impl() const override { std::cout << "foo in DerivedA\n"; }
+};
+
+class DerivedB : public IBase {
+private:
+  void foo_impl() const override { std::cout << "foo in DerivedB\n"; }
+};
+
+void bar(const IBase &base) { base.foo(); }
+
+int main(int /*unused*/, char ** /*unused*/) {
+  DerivedA a;
+  DerivedB b;
+
+  bar(a);
+  bar(b);
+}
