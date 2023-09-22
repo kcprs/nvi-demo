02fcee70f540b44a7fc67b07faeab34c05468cde
Kacper Sagnowski <ksagnowski@gmail.com>
2023-09-22 15:01:43 +0200

    Add example of buggy implementation

--- main.cpp
+++ main.cpp
@@ -60,3 +60,55 @@ int main(int /*unused*/, char ** /*unused*/) {
   bar(a);
   bar(b);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//                    Example of buggy implementation
+////////////////////////////////////////////////////////////////////////////////
+
+// #include <algorithm>
+// #include <set>
+// #include <vector>
+
+// // Example from
+// // https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-Virtual_Interface
+
+// class Set {
+//   std::set<int> s_;
+
+// public:
+//   void add(int i) {
+//     s_.insert(i);
+//     add_impl(i); // Note virtual call.
+//   }
+//   void addAll(int *begin, int *end) {
+//     s_.insert(begin, end); // OK
+//     // std::for_each(begin, end, [this](auto i) { add(i); }); // Buggy
+
+//     addAll_impl(begin, end); // Note virtual call.
+//   }
+
+// private:
+//   virtual void add_impl(int i) = 0;
+//   virtual void addAll_impl(int *begin, int *end) = 0;
+// };
+// class CountingSet : public Set {
+// public:
+//   int count() const { return count_; }
+
+// private:
+//   int count_;
+//   virtual void add_impl(int i) { count_++; }
+//   virtual void addAll_impl(int *begin, int *end) {
+//     count_ += std::distance(begin, end);
+//   }
+// };
+
+// int main(int /*unused*/, char ** /*unused*/) {
+// std:
+//   std::vector<int> v{1, 2, 3, 4, 5};
+//   CountingSet s;
+
+//   s.addAll(v.begin().base(), v.end().base());
+
+//   std::cout << "Count is " << s.count() << std::endl;
+// }
